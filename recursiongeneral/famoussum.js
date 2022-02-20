62bb32c1ed434194e49284eddd90be9ad625ef01
Prathyu Prasad PK <67427506+prathyu116@users.noreply.github.com>
2022-02-20 21:20:52 +0530

    Update famoussum.js

--- recursiongeneral/famoussum.js
+++ recursiongeneral/famoussum.js
@@ -1,1 +1,23 @@
-dfdd
+function runProgram(input){
+     input = input.trim().split("\n");
+
+let n=+input[0];
+for(let i=1;i<=n;i++){
+    let num=input[i];
+    find(num);
+    }
+}
+
+function find(num){
+   if(num.length==1){
+       console.log(num);
+       return;
+   }
+   let sum=0;
+   for(let i=0;i<num.length;i++){
+       sum+=(Number)(num[i]);
+   }
+   find(sum.toString());
+    
+     
+}
