01852247f1d867c08281ba6476fb320e9c65f9fb
Prathyu Prasad PK <67427506+prathyu116@users.noreply.github.com>
2022-02-21 00:14:49 +0530

    Create Make all combinations of size k.js

---
+++ Recursion&backTracking/Make all combinations of size k.js
@@ -0,0 +1,34 @@
+function runProgram(input) {
+  input = input.trim().split(" ");
+  var N = +input[0];
+  var K=+input[1]
+  
+  BirthdayShopping(N,1,K)
+
+}
+
+let ans = [];
+let tmp = [];
+         
+    function BirthdayShopping(n, left, k)
+    {
+        if (k == 0) {
+            ans.push(tmp);
+            bag=""
+            for(let i = 0; i < tmp.length; i++)
+            {
+              bag=bag+tmp[i]+" "
+            }
+            console.log(bag)
+            
+            return;
+        }
+ 
+
+        for (let i = left; i <= n; i++)
+        {
+            tmp.push(i);
+            BirthdayShopping(n, i + 1, k - 1);
+            tmp.pop();
+        }
+    }
