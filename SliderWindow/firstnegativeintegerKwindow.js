ab441e3bf610982475f5bc727bd2d480d52e1d7e
Prathyu Prasad PK <67427506+prathyu116@users.noreply.github.com>
2022-02-27 18:30:04 +0530

    Create firstnegativeintegerKwindow.js

---
+++ SliderWindow/firstnegativeintegerKwindow.js
@@ -0,0 +1,41 @@
+function FirstNegative(N, k, arr) {
+  var list = []
+  var arr2 = []
+
+  var i = 0
+  var j = 0
+  while (j < arr.length) {
+    if (arr[j] < 0) {
+      list.push(arr[j])
+    }
+    
+    if (j - i + 1 < k) {
+      j++
+    }
+    else if (j - i + 1 == k) {
+      if (list.length != 0) {
+        arr2.push(list[0])
+        if (arr[i] == list[0]) {
+             list.shift()
+        }
+
+      } else {
+        arr2.push(0)
+
+      }
+      i++
+      j++
+
+    }
+
+
+  }
+  console.log(arr2)
+
+
+}
+var s = 5, k = 2
+var arr = [-8, 2, 3, -6, 10]
+
+FirstNegative(s, k, arr)
+
