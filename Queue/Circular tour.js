1323d6aa7e99503c2a10c1d2bd30f4c42685a0cc
Prathyu Prasad PK <67427506+prathyu116@users.noreply.github.com>
2022-03-01 20:40:53 +0530

    Create Circular tour.js

---
+++ Queue/Circular tour.js
@@ -0,0 +1,26 @@
+//https://practice.geeksforgeeks.org/problems/circular-tour/1
+
+N = 4
+var Petrol = [4, 6, 7, 4]
+var Distance = [6, 5, 3, 5]
+var ans=CircularTour(Petrol,Distance,N)
+console.log(ans)
+function CircularTour(Petrol, Distance, N) {
+  var avoiding = 0
+  var balance = 0
+  var start = 0
+  
+  for (var i = 0; i < N; i++) {
+    balance = balance + (Petrol[i] - Distance[i])
+    if (balance < 0) {
+      avoiding = avoiding + balance
+      start = i + 1
+      balance = 0
+    }
+  }
+  if (avoiding + balance >= 0) {
+    return start
+  } else {
+    return -1
+  }
+}
