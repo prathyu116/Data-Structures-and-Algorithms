c8f0a46b37a6206c18074ee045aafc8c371dfd54
Prathyu Prasad PK <67427506+prathyu116@users.noreply.github.com>
2022-03-06 23:53:46 +0530

    Create SqureRootBS.js

---
+++ BinarySearch/SqureRootBS.js
@@ -0,0 +1,25 @@
+//in log(n) complexity
+// sqrt(36)==>6
+
+function SqureRoot(n){
+    var s=0;
+    var e=n;
+    var ans=-1;
+        while(s<=e){
+                var mid=parseInt((s+(e-s)/2) )
+
+          var squre=mid*mid;
+            if(squre==n){
+                console.log(mid)
+                return 
+            } 
+            if(squre < n) {
+                ans=mid;
+                s=mid+1;
+            }else{
+                 e=mid-1;
+            }
+        }
+   console.log(ans)
+   return 
+}
