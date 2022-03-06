7dc5c5675c7893edd5f8f7a602f68489e16de66c
Prathyu Prasad PK <67427506+prathyu116@users.noreply.github.com>
2022-03-06 20:30:00 +0530

    Create PivotElement.js

---
+++ BinarySearch/PivotElement.js
@@ -0,0 +1,22 @@
+//https://replit.com/@PrathyuPrasad/UnripeDarkmagentaLightweightprocess#index.js:1:9
+//pivot elemtn--down th first monotomic function
+
+
+function Pivot(n, arr) {
+  var s = 0; var e = n - 1;
+
+  while (s < e) {
+    var mid = (s + e) / 2;
+
+    if (arr[mid] > arr[0]) {
+      s = mid + 1;
+    } else {
+      e = mid;
+    }
+  }
+  return s;
+}
+
+var arr = [8, 10, 17, 1, 3]
+var ans = Pivot(arr.length, arr)
+console.log(ans)
