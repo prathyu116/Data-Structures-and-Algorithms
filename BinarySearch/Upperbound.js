ff48dce7387a50e5747aeae0977b0964eb8c83ae
Prathyu Prasad PK <67427506+prathyu116@users.noreply.github.com>
2022-03-12 20:04:17 +0530

    Create Upperbound.js

---
+++ BinarySearch/Upperbound.js
@@ -0,0 +1,24 @@
+//TC O(log n)
+//UPPERBOUND----> arr[i] > key(Just >than key )
+
+function upperBound(arr,key) {
+  var start = 0, end = arr.length - 1, UpperIndex = -1
+  while (start < end) {
+    var mid = Math.floor(start + (end - start) / 2)
+
+    if (arr[mid] <= key) {
+      
+      start = mid + 1
+    } else {
+      UpperIndex=mid
+      end=mid-1
+    } 
+  }
+
+  return UpperIndex
+}
+var key = 3
+var arr = [0 ,2 ,4 ,4, 5, 5 ,7 ,7 ,9 ,10]
+
+var ans = upperBound(arr,key)
+console.log(ans)
