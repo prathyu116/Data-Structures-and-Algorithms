30f4df622389f169cfc0a8ded41167307005c983
Prathyu Prasad PK <67427506+prathyu116@users.noreply.github.com>
2022-03-14 01:09:07 +0530

    Create CandyDistribution.js

---
+++ Greedy/CandyDistribution.js
@@ -0,0 +1,32 @@
+// https://www.youtube.com/watch?v=h6_lIwZYHQw
+
+function DistributeCandies(n,arr){
+
+    let ltr = new Array(n).fill(1);
+  
+    for(let i = 1; i < n ; i++)
+    {
+        if (arr[i] > arr[i-1])
+        {
+
+            ltr[i] = ltr[i-1] + 1;
+        }
+    }
+    
+    let rtl = new Array(n).fill(1);
+  
+    for(let i = n-2; i >=0 ; i--)
+    {
+        if (arr[i] > arr[i+1])
+        {
+
+            rtl[i] = rtl[i+1] + 1;
+        }
+    }
+   var sum = 0
+    for(var i=0;i<ltr.length;i++){
+        sum=sum+ Math.max(ltr[i],rtl[i])
+    }
+  console.log(sum)
+   
+}
