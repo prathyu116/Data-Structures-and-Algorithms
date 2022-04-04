b9d3b19d1a5bf1aa20f4408af8d8ab8894620263
Prathyu Prasad PK <67427506+prathyu116@users.noreply.github.com>
2022-04-04 22:50:51 +0530

    Update BuyMaximumstocksBoughtOn i-thDay.js

--- Greedy/BuyMaximumstocksBoughtOn i-thDay.js
+++ Greedy/BuyMaximumstocksBoughtOn i-thDay.js
@@ -21,3 +21,32 @@ function MaximumStocks(arr,k){
 var arr = [7, 10, 4 ]
 var k=100
 MaximumStocks(arr,k)
+
+
+-----------------------------------
+
+function buyMaximumProducts(price,k){
+var arr=[],ans=0
+  for(var i=0;i<price.length;i++){
+    arr.push([price[i],i+1])
+  }
+  arr.sort((a,b)=>a[0]-b[0]) //we want max profit so sort inaccending order
+
+  for(var i=0;i<arr.length;i++){
+    var price=arr[i][0]
+    var stock=arr[i][1]
+    if(price*stock <=k){
+      ans=ans+stock
+      k=k-(price*stock)
+    }else{
+      ans=ans+Math.floor((k/price))
+      k=k-price * Math.floor((k/price))
+    }
+  }
+console.log(ans)
+}
+var K = 45
+var price = [10, 7, 19 ]
+buyMaximumProducts(price,K)
+
+
