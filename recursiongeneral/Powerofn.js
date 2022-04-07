c7fa00fc5a14abbbcb5ccc5a1c06faf6a9e3de65
Prathyu Prasad PK <67427506+prathyu116@users.noreply.github.com>
2022-04-07 20:39:20 +0530

    Create Powerofn.js

---
+++ recursiongeneral/Powerofn.js
@@ -0,0 +1,20 @@
+/*
+
+*/
+
+function poewer(n){
+if(n==0) return 1
+return 2*power(n-1)
+}
+
+
+function PowerFunctionRecursive(arr){
+    var a=arr[0]
+    var b=arr[1]
+    var ans=1
+    for(var i=1;i<=b;i++){
+        ans=ans*a
+    }
+  console.log(ans)
+    
+}
