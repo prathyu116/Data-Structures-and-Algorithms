fc64d55a3f463376384c4be235949d81b602c8df
Prathyu Prasad PK <67427506+prathyu116@users.noreply.github.com>
2022-04-09 01:11:49 +0530

    Update SubsetSum.js

--- Recursion&backTracking/SubsetSum.js
+++ Recursion&backTracking/SubsetSum.js
@@ -1,23 +1,32 @@
- generate_subsequence(arr,sum,i=0,K)
-  if(ans){
-      console.log("yes")
-  }else{
-      console.log("no")
-  }
- 
-  
-    
 }
 function generate_subsequence(s , sub , index,k){
   if(index==s.length){
-    
+
     return sub==k
     
 }
   var x =generate_subsequence(s,sub+s[index],index+1,k)
   if(x==true) return true
   var y=generate_subsequence(s,sub,index+1,k)
-  if(y==true) return true
-    
+  return y
    
 }
+
+//https://recursion.vercel.app/  dry run on site
+/*
+function fn(s , sub , index,k) {
+   if(index==s.length){
+
+    return sub==k
+    
+}
+  var x =fn(s,sub+s[index],index+1,k)
+  if(x==true) return true
+  var y=fn(s,sub,index+1,k)
+  return y
+   
+}
+
+fn([1,2,3],0,i=0,6)
+
+*/
