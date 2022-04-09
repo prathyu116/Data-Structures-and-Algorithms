ebb8d26492ed0566ae8e826d34229a21857507c5
Prathyu Prasad PK <67427506+prathyu116@users.noreply.github.com>
2022-04-10 02:40:41 +0530

    Create missingInteger.js

---
+++ ARRAY/missingInteger.js
@@ -0,0 +1,9 @@
+var arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,15,16,17,18,19,20]
+
+for(let i = 0; i < arr.length-1; i++){
+    if(arr[i] + 1 == arr[i+1]){
+
+    } else {
+        console.log(arr[i] + 1)
+    }
+}
