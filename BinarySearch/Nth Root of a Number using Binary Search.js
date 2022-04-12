ac76e39addc33c2e47ea8f9000f9da24b107fd0c
Prathyu Prasad PK <67427506+prathyu116@users.noreply.github.com>
2022-04-12 23:43:58 +0530

    Update Nth Root of a Number using Binary Search.js

--- BinarySearch/Nth Root of a Number using Binary Search.js
+++ BinarySearch/Nth Root of a Number using Binary Search.js
@@ -1,1 +1,29 @@
 //https://takeuforward.org/data-structure/nth-root-of-a-number-using-binary-search/
+//https://replit.com/@PrathyuPrasad/InternalSuspiciousPrinters#index.js
+
+
+function multiply(number, n) {
+  var ans = 1;
+  for (var i = 1; i <= n; i++) {
+    ans = ans * number;
+  }
+  return ans;
+}
+
+function aqrt(n, m) {
+  var low = 1;
+  var high = m;
+  var eps = 0.000001; // used for taking approximations of the answer
+
+  while ((high - low) > eps) {
+    var mid = (((low + high) / 2));
+    if (multiply(mid, n) < m) {
+      low = mid;
+    }
+    else {
+      high = mid;
+    }
+  }
+  console.log(low.toFixed(3))
+}
+aqrt(3, 7)
