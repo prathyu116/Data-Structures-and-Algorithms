053b57140a9fb180ec25673e79f613c9b9990c9a
Prathyu Prasad PK <67427506+prathyu116@users.noreply.github.com>
2022-04-16 15:49:12 +0530

    Create Masai Palindromic Substring.js

---
+++ OJ/Masai Palindromic Substring.js
@@ -0,0 +1,33 @@
+function masaiPalSubString(S){
+    //write code here
+    var max=0;
+    for( var i=0;i<S.length;i++){
+        for(var j=0;j<S.length;j++){
+            var bag="";
+            for(var k=i;k<=j;k++){
+                bag=bag+S[k]
+            }
+            var new_bag=bag.split("").reverse().join("");
+            
+            if(new_bag==bag){
+                 if(new_bag.length > max){
+                     max=new_bag.length;
+                 }
+            }
+           
+        }
+    }
+    console.log(max)
+}
+
+
+
+
+
+
+
+
+
+
+
+
