096824d51637ceb4bd84824c15a85123dc4464c9
Prathyu Prasad PK <67427506+prathyu116@users.noreply.github.com>
2022-04-18 02:22:13 +0530

    Update LongestCommanSubsequnce.js

--- DP/LongestCommanSubsequnce.js
+++ DP/LongestCommanSubsequnce.js
@@ -1,1 +1,8 @@
-k
+https://oj.masaischool.com/contest/3603/problem/002
+function LongestCommanSubseq(s1,s2,i,j){
+  if(i >=s1.length || j >=s2.length) return 0
+  if(s1[i]===s2[j]) return 1+LongestCommanSubseq(s1,s2,i+1,j+1)
+  else return (Math.max(LongestCommanSubseq(s1,s2,i+1,j),LongestCommanSubseq(s1,s2,i,j+1)))
+}
+var ans=LongestCommanSubseq(s1,s2,i=0,j=0)
+console.log(ans)
