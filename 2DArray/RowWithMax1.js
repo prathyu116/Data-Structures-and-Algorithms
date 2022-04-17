d028ccf318ea509654af1bec07197b3514cc1aca
Prathyu Prasad PK <67427506+prathyu116@users.noreply.github.com>
2022-04-17 12:18:38 +0530

    Create RowWithMax1.js

---
+++ 2DArray/RowWithMax1.js
@@ -0,0 +1,22 @@
+var items = [];
+
+items.push([0,1,1,1]);
+items.push([0,0,1,1]);
+items.push([0,0,1,1]);
+items.push([1,1,1,1]);
+
+var m=4,n=4;
+
+function hai(){
+ for(var i=0;i<m;i++)
+    {
+        for(var j=0;j<n;j++)
+        {
+            if(items[j][i]==1)
+            return j;
+        }
+    }
+    return -1;
+}
+var ans = hai(items,n,m)
+console.log(ans)
