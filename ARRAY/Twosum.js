bb7adceba8acb7e896ff5b9333537fd2a8770280
Prathyu Prasad PK <67427506+prathyu116@users.noreply.github.com>
2022-04-17 16:56:16 +0530

    Create Twosum.js

---
+++ ARRAY/Twosum.js
@@ -0,0 +1,18 @@
+/**
+ Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+ */
+var twoSum = function(nums, target) {
+    let obj = {}
+    for (i=0;i<nums.length;i++){
+        let diff = target-nums[i]
+        if(obj[diff] != null){
+            return [obj[diff],i]
+        }else{
+            obj[nums[i]] = i
+        }
+    }
+};
+
+//search in object time complexity O(1)
