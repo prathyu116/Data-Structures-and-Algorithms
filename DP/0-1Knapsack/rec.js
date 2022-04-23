af840071fc83568b724148d3aa0673a97c5806d3
Prathyu Prasad PK <67427506+prathyu116@users.noreply.github.com>
2022-04-23 12:29:18 +0530

    Create rec.js

---
+++ DP/0-1Knapsack/rec.js
@@ -0,0 +1,63 @@
+/*
+Recursion code
+Time Complexity: O(2^n)
+*/
+
+	function knapSack(W, wt, val, n)
+	{
+		// Base Case
+		if (n == 0 || W == 0)
+			return 0;
+
+		// If weight of the nth item is
+		// more than Knapsack capacity W,
+		// then this item cannot be
+		// included in the optimal solution
+		if (wt[n - 1] <= W)
+			return Math.max(val[n - 1] +knapSack(W - wt[n - 1], wt, val, n - 1),knapSack(W, wt, val, n - 1)) //include aya value + bakki ulllath excute cheyyan
+
+		// Return the maximum of two cases:
+		// (1) nth item included
+		// (2) not included
+		else
+			return knapSack(W, wt, val, n - 1)
+	}
+		
+	let val = [ 60, 100, 120 ];
+	let wt = [ 10, 20, 30 ];
+	let W = 50;
+	let n = val.length;
+
+	var ans=knapSack(W, wt, val, n)
+	console.log(ans)/*
+Recursion code
+Time Complexity: O(2^n)
+*/
+
+	function knapSack(W, wt, val, n)
+	{
+		// Base Case
+		if (n == 0 || W == 0)
+			return 0;
+
+		// If weight of the nth item is
+		// more than Knapsack capacity W,
+		// then this item cannot be
+		// included in the optimal solution
+		if (wt[n - 1] <= W)
+			return Math.max(val[n - 1] +knapSack(W - wt[n - 1], wt, val, n - 1),knapSack(W, wt, val, n - 1)) //include aya value + bakki ulllath excute cheyyan
+
+		// Return the maximum of two cases:
+		// (1) nth item included
+		// (2) not included
+		else
+			return knapSack(W, wt, val, n - 1)
+	}
+		
+	let val = [ 60, 100, 120 ];
+	let wt = [ 10, 20, 30 ];
+	let W = 50;
+	let n = val.length;
+
+	var ans=knapSack(W, wt, val, n)
+	console.log(ans)
