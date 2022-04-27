c61e5b609547f3078015fadcc22bc15f93582418
Prathyu Prasad PK <67427506+prathyu116@users.noreply.github.com>
2022-04-27 21:46:41 +0530

    Create preorder.js

---
+++ Binary Tree/preorder.js
@@ -0,0 +1,34 @@
+/*
+
+
+https://takeuforward.org/data-structure/preorder-traversal-of-binary-tree/
+
+*/
+var preorderTraversal = function(root) {
+    let output = [];
+    function foo (node) {
+        if(!node) return;
+        output.push(node.val);
+        foo(node.left);
+        foo(node.right);
+    }
+    foo(root);
+    return output;
+};
+
+-----------------------------itreative
+var preorderTraversal = function(node) {
+    let stack = [];
+    let output = [];
+    while (stack.length || node !== null) {
+        if (node !== null) {
+            output.push(node.val);
+            stack.push(node);
+            node = node.left;
+        } else {
+            node = stack.pop().right;
+        }
+        
+    }
+    return output;
+};
