8ce32ec44f54683513bfcef22f20a0c856a2efaf
Prathyu Prasad PK <67427506+prathyu116@users.noreply.github.com>
2022-05-09 20:22:32 +0530

    Update Maximum depth of a Binary Tree.js

--- Binary Tree/Maximum depth of a Binary Tree.js
+++ Binary Tree/Maximum depth of a Binary Tree.js
@@ -1,3 +1,21 @@
 
-
+/*
 https://takeuforward.org/data-structure/maximum-depth-of-a-binary-tree/
+we caan use recursive traversal
+*/
+
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+var maxDepth = function(root) {
+     if (!root) return 0; 
+    return 1 + Math.max(maxDepth(root.left), maxDepth(root.right))
+};
+
+Time Complexity: O(N) 
+
+Space Complexity: O(1) Extra Space + O(H) Recursion Stack space, where “H”  is the height of the binary tree.
