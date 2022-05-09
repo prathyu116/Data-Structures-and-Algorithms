6033474a78d4c00e6e35ca6c3f3e043fc40a502f
Prathyu Prasad PK <67427506+prathyu116@users.noreply.github.com>
2022-05-09 21:09:15 +0530

    Update balancedBT.js

--- Binary Tree/balancedBT.js
+++ Binary Tree/balancedBT.js
@@ -1,5 +1,29 @@
 /*
 https://takeuforward.org/data-structure/check-if-the-binary-tree-is-balanced-binary-tree/
+
 BalancedBT : Absolute( Left Subtree Height – Right Subtree Height ) <= 1
 
 */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isBalanced = function(root) {
+        let dfs = function(node) {
+        if (!node) return 0;
+        let left = 1 + dfs(node.left);
+        let right = 1 + dfs(node.right);
+        if (Math.abs(left - right) > 1) return Infinity;
+        return Math.max(left, right);
+    }
+    
+    return dfs(root)==Infinity?false:true;
+};
