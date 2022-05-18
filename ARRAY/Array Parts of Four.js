89a35e09f45c8dacf0404aee503952610abf6902
Prathyu Prasad PK <67427506+prathyu116@users.noreply.github.com>
2022-05-19 01:47:55 +0530

    Create Array Parts of Four.js

---
+++ ARRAY/Array Parts of Four.js
@@ -0,0 +1,11 @@
+function check(N,arr){
+// Write your code here!
+var chunkSize=N/4
+var ans =[]
+for (let i = 0; i < arr.length; i += chunkSize) {
+  const chunk = arr.slice(i, i + chunkSize);
+ var sum= chunk.reduce((partialSum, a) => partialSum + a, 0);
+ ans.push(sum)
+}
+console.log(2*ans[0] + ans[1] + 2*ans[2] + ans[3] )
+}
