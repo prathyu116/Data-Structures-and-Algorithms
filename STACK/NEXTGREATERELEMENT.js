a661a023fc8cfcc25716804e8897eb70c04e7a6f
Prathyu Prasad PK <67427506+prathyu116@users.noreply.github.com>
2022-05-22 02:54:47 +0530

    Update NEXTGREATERELEMENT.js

--- STACK/NEXTGREATERELEMENT.js
+++ STACK/NEXTGREATERELEMENT.js
@@ -19,3 +19,55 @@ var nextGreaterElement = function(nums1, nums2) {
     return result;
 };
 --------------------------------------------------
+function NGR(n, arr) {
+  var stack = []
+  var ans = []
+  for (var i = n-1; i >=0; i--) {
+    while (stack.length != 0 && stack[stack.length - 1] <= arr[i]) {
+      stack.pop()
+    }
+    if (stack.length == 0) {
+      ans.push(-1)
+    } else {
+      ans.push(stack[stack.length - 1])
+    }
+    stack.push(arr[i])
+  }
+  console.log(ans.reverse().join(" "))
+}
+
+
+var arrr = [4,5,2,10,8]
+var s = arrr.length
+NGR(s, arrr)
+
+
+
+------------------------------------------------NEXT GREATEST CIRCULAR-------------------------------------------------
+    function NGCIRCULAR(n, arr) {
+  var stack = []
+  var ans = []
+  for (var i = 2 * n - 1; i >= 0; i--) {
+    while (stack.length != 0 && stack[stack.length - 1] <= arr[i % n]) {
+      stack.pop();
+    }
+
+    if (i < n) {
+      if (stack.length == 0) {
+        ans.push(-1)
+      } else {
+        ans.push(stack[stack.length - 1])
+      }
+    }
+    stack.push(arr[i % n])
+  }
+  console.log(ans.reverse())
+}
+
+
+var arrr = [2,10,12,1,11] //[2,10,12,1,11] total 9 ele ulla array akkum  ...5th index nte value 2 nn kiitum...how ? 5%5..and for 6th index 6%5
+var s = arrr.length
+NGCIRCULAR(s, arrr)
+
+
+
