4b45c5486d0bf145410351e5e9a013a8505e7e16
Prathyu Prasad PK <67427506+prathyu116@users.noreply.github.com>
2022-05-22 03:02:30 +0530

    Update NEARESTSMALLESTLEFT.js

--- STACK/NEARESTSMALLESTLEFT.js
+++ STACK/NEARESTSMALLESTLEFT.js
@@ -25,3 +25,26 @@ function NSL(n, arr) {
 var arrr = [4,5,2,10,8]
 var s = arrr.length
 NSL(s, arrr)
+
+------------------------------------------NEAREST GREATEST LEFT--------------------------------------------------------------
+function NGL(n, arr) {
+  var stack = []
+  var ans = []
+  for (var i = 0; i < n; i++) {
+    while (stack.length != 0 && stack[stack.length - 1] <= arr[i]) {
+      stack.pop()
+    }
+    if (stack.length == 0) {
+      ans.push(-1)
+    } else {
+      ans.push(stack[stack.length - 1])
+    }
+    stack.push(arr[i])
+  }
+  console.log(ans.join(" "))
+}
+
+
+var arrr = [1,3,2,4]
+var s = arrr.length
+NGL(s, arrr)
