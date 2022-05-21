329eca116512d8fe61ab340894f8cf1062772152
Prathyu Prasad PK <67427506+prathyu116@users.noreply.github.com>
2022-05-22 02:36:14 +0530

    Update NEARESTSMALLESTLEFT.js

--- STACK/NEARESTSMALLESTLEFT.js
+++ STACK/NEARESTSMALLESTLEFT.js
@@ -4,3 +4,24 @@
 
 o/p=>-1 -1 -1 -1 4 24 24 -1
 */
+function NSL(n, arr) {
+  var stack = []
+  var ans = []
+  for (var i = 0; i < arr.length; i++) {
+    while (stack.length != 0 && stack[stack.length - 1] >= arr[i]) {
+      stack.pop()
+    }
+    if (stack.length == 0) {
+      ans.push(-1)
+    } else {
+      ans.push(stack[stack.length - 1])
+    }
+    stack.push(arr[i])
+  }
+  console.log(ans.join(" "))
+}
+
+
+var arrr = [4,5,2,10,8]
+var s = arrr.length
+NSL(s, arrr)
