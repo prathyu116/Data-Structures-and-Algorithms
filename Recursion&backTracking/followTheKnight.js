8f0db6a718325c1e610987552f48bb0319abdc40
Prathyu Prasad PK <67427506+prathyu116@users.noreply.github.com>
2022-05-25 21:27:48 +0530

    Update followTheKnight.js

--- Recursion&backTracking/followTheKnight.js
+++ Recursion&backTracking/followTheKnight.js
@@ -56,25 +56,3 @@ function FollowTheKnight(matrix,row,col,N){
 }
 
 
- 
-
-if (process.env.USER === "") {
-  runProgram(``);
-} else {
-  process.stdin.resume();
-  process.stdin.setEncoding("ascii");
-  let read = "";
-  process.stdin.on("data", function (input) {
-    read += input;
-  });
-  process.stdin.on("end", function () {
-    read = read.replace(/\n$/, "");
-    read = read.replace(/\n$/, "");
-    runProgram(read);
-  });
-  process.on("SIGINT", function () {
-    read = read.replace(/\n$/, "");
-    runProgram(read);
-    process.exit(0);
-  });
-}
