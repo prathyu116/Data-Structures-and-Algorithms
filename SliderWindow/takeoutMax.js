93535e1bbf03af7089e8b8523fa5f6d5e6ae5c6e
Prathyu Prasad PK <67427506+prathyu116@users.noreply.github.com>
2022-06-01 23:02:38 +0530

    Update takeoutMax.js

--- SliderWindow/takeoutMax.js
+++ SliderWindow/takeoutMax.js
@@ -21,3 +21,25 @@ function Takeoutmaximum(N,k,arr){
 var s=10,k= 3
 var arr=[-1 ,-1, -2, 1, -2, 4 ,1 ,9 ,3 ,9]
 Takeoutmaximum(s,k,arr)
+
+
+
+
+
+function Takeoutmaximum(N,k,arr){
+    
+     var max=-999999999999999999
+     var sum=0
+     for(var i=0;i<k;i++){
+         sum += arr[i]
+        
+     }
+    max=Math.max(sum,max)
+     for(var i=k;i<N;i++){
+         sum=sum+arr[i]
+         sum=sum-arr[i-k]
+         max=Math.max(sum,max)
+     }
+     console.log(max)
+    
+}
