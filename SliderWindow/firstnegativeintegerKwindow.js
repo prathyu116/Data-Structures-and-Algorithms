5211d0b9eb745f7559e03008143d94dd0bf36dff
Prathyu Prasad PK <67427506+prathyu116@users.noreply.github.com>
2022-06-01 23:25:11 +0530

    Update firstnegativeintegerKwindow.js

--- SliderWindow/firstnegativeintegerKwindow.js
+++ SliderWindow/firstnegativeintegerKwindow.js
@@ -1,45 +1,35 @@
 //push all negative number in window size k into list
 //push to first elemtn of list to arr2
-
-
 function FirstNegative(N, k, arr) {
-  var list = []
-  var arr2 = []
-
-  var i = 0
-  var j = 0
+  var list = [];
+  var arr2 = [];
+  var i = 0;
+  var j = 0;
   while (j < arr.length) {
     if (arr[j] < 0) {
-      list.push(arr[j])
+      list.push(arr[j]);
     }
-    
     if (j - i + 1 < k) {
-      j++
-    }
-    else if (j - i + 1 == k) {
+      j++;
+    } else if (j - i + 1 == k) {
       if (list.length != 0) {
-        arr2.push(list[0])
+        arr2.push(list[0]);
         if (arr[i] == list[0]) {
-             list.shift()
+          list.shift();
         }
-
       } else {
-        arr2.push(0)
-
+        arr2.push(0);
       }
-      i++
-      j++
-
+      i++;
+      j++;
     }
-
-
   }
-  console.log(arr2)
-
-
+  console.log(arr2);
 }
-var s = 5, k = 2
-var arr = [-8, 2, 3, -6, 10]
+var s = 5,
+  k = 2;
+var arr = [-8, 2, 3, -6, 10];
 
-FirstNegative(s, k, arr)
+FirstNegative(s, k, arr);
+
 
