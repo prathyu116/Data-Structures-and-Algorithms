a022c108f195ac010c29e03537eb492a7eeb0d78
Prathyu Prasad PK <67427506+prathyu116@users.noreply.github.com>
2022-06-09 14:17:20 +0530

    Create lowerbound.js

---
+++ BinarySearch/lowerbound.js
@@ -0,0 +1,17 @@
+function UpperBound(arr,key){
+    var start=0,end=arr.length-1,first=-1
+    while(start<=end){
+        var mid=Math.floor(start+(end-start)/2)
+
+        if(arr[mid]==key) {
+            first=mid  //for first occurance we move to left
+            end=mid-1
+        }else if(key > arr[mid]){
+            start=mid+1
+        }else if(key < arr[mid]){
+            end=mid-1
+        }
+    }
+
+    return first
+}
