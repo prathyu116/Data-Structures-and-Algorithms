4f1edec191a6dc484a41c3e6d52230f5432b7738
Prathyu Prasad PK <67427506+prathyu116@users.noreply.github.com>
2022-06-27 22:22:20 +0530

    Update 1.subsequnce.js

--- recursiongeneral/TUF/1.subsequnce.js
+++ recursiongeneral/TUF/1.subsequnce.js
@@ -13,3 +13,37 @@ function subsequnce(index,list,arr){
 subsequnce(0,[],[1,2,3])
 
 evvry index have 2 option ..theefore exponential
+
+
+
+
+
+
+
+
+
+
+maaii
+
+
+
+//Enter code here
+function generate_subsequence(s , sub , index){
+    
+        if(sub.length != 0){
+            console.log(sub.join(""))
+        }
+        for(let i = index ; i < s.length ; i++){
+            sub.push(s[i]);
+            generate_subsequence(s , sub , i + 1)
+            sub.pop()
+        }
+}
+function runProgram(input){
+    var input=input.split("\n")
+    var str = input[1]
+let output = []
+generate_subsequence(str ,output , i =0)
+
+
+}
