a773a99a727c16d87623f9781b81b448dfa70979
Prathyu Prasad PK <67427506+prathyu116@users.noreply.github.com>
2025-01-09 16:30:08 +0530

    Update lowerbound.js

--- BinarySearch/lowerbound.js
+++ BinarySearch/lowerbound.js
@@ -1,18 +1,26 @@
-function LowerBound(arr,key){
-    var start=0,end=arr.length-1,first=-1
-    while(start<=end){
-        var mid=Math.floor(start+(end-start)/2)
 
-        if(arr[mid]==key) {
-            first=mid  //for first occurance we move to left
-            end=mid-1
-        }else if(key > arr[mid]){
-            start=mid+1
-        }else if(key < arr[mid]){
-            end=mid-1
+function lowerBound(arr, n, x) {
+    let low = 0, high = n - 1;
+    let ans = n;
+
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2);
+        // maybe an answer
+        if (arr[mid] >= x) {
+            ans = mid;
+            // look for smaller index on the left
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1; // look on the right
         }
     }
-
-    return first
+    return ans;
 }
+
+let arr = [3, 5, 8, 15, 19];
+let n = 5, x = 9;
+let ind = lowerBound(arr, n, x);
+console.log("The lower bound is the index:", ind);
+
 //lower bound means the smallest index such that ..that number >= target
