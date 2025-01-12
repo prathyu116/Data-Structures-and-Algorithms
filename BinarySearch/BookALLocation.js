e53341da7a4dc49015eeb92573d037fd3792cf40
Prathyu Prasad PK <67427506+prathyu116@users.noreply.github.com>
2025-01-13 01:57:05 +0530

    Update BookALLocation.js

--- BinarySearch/BookALLocation.js
+++ BinarySearch/BookALLocation.js
@@ -83,3 +83,112 @@ console.log(ans)
 
 Time Complexity: O(N*log M), where N is the number of different books and M denotes the sum of number of pages of all different books
 Auxiliary Space: O(1)
+
+
+
+
+
+
+
+
+
+
+
+-------------------------------------------------------------------
+        
+
+
+function countStudents(arr, pages) {
+  var n = arr.length; // size of array
+  var students = 1;
+  var pagesStudent = 0;
+  for (var i = 0; i < n; i++) {
+    if (pagesStudent + arr[i] <= pages) {
+      // add pages to current student
+      pagesStudent += arr[i];
+    } else {
+      // add pages to next student
+      students++;
+      pagesStudent = arr[i];
+    }
+  }
+  return students;
+}
+
+function findPages(arr, n, m) {
+  // book allocation impossible
+  if (m > n) return -1;
+
+  var low = Math.max(...arr);
+  var high = arr.reduce((a, b) => a + b, 0);
+  while (low <= high) {
+    var mid = Math.floor((low + high) / 2);
+    var students = countStudents(arr, mid);
+    if (students > m) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
+  }
+  return low;
+}
+
+var arr = [25, 46, 28, 49, 24];
+var n = 5;
+var m = 4;
+var ans = findPages(arr, n, m);
+console.log("The answer is: " + ans);
+
+//mid=110...
+
+/*for student 1 25+46+28 
+student 2 49+24
+but i need to do with 4 students..with 110 required 2 studnet..anytthing on right will not our answer..so reduce th search space
+*/
+
+
+
+brute force
+
+
+
+function countStudents(arr, pages) {
+    var n = arr.length; // size of array
+    var students = 1;
+    var pagesStudent = 0;
+    for (var i = 0; i < n; i++) {
+        if (pagesStudent + arr[i] <= pages) {
+            // add pages to current student
+            pagesStudent += arr[i];
+        } else {
+            // add pages to next student
+            students++;
+            pagesStudent = arr[i];
+        }
+    }
+    return students;
+}
+
+function findPages(arr, n, m) {
+    // book allocation impossible
+    if (m > n) return -1;
+
+    var low = Math.max(...arr);
+    var high = arr.reduce((a, b) => a + b, 0);
+
+    for (var pages = low; pages <= high; pages++) {
+        if (countStudents(arr, pages) === m) {
+            return pages;
+        }
+    }
+    return low;
+}
+
+var arr = [25, 46, 28, 49, 24];
+var n = 5;
+var m = 4;
+var ans = findPages(arr, n, m);
+console.log("The answer is: " + ans);
+
+
+
